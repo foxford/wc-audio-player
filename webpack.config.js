a29bcf0727ead65b0bd423a06c21374bda83466e
Aleksey Konstantinov <a.konstantinov@netology-group.ru>
2018-07-29 18:46:25 +0300

    MEDIA-1594 Added 'audio-player' web component

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: {
+    'audio-player': './src/audio-player.js'
+  },
+  output: {
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'postcss-loader'
+        ]
+      }
+    ]
+  }
+}
